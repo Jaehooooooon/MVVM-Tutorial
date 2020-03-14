36625657ef583bfb74f09f37de132b419e98a14a
Jaehooooooon <jaehoon572@naver.com>
2020-03-14 23:28:34 +0900

    Initialize

---
+++ MVVMTutorial/HomeViewController.swift
@@ -0,0 +1,30 @@
+//
+//  HomeViewController.swift
+//  MVVMTutorial
+//
+//  Created by 서재훈 on 2020/03/14.
+//  Copyright © 2020 서재훈. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
